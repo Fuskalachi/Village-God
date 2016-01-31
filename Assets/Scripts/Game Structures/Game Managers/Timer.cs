799a082d3e29eab1b3326cccdc12003161cebd23
Michael Marcelo <miccelo@gmail.com>
2016-01-30 19:55:07 -0500

    Add Timer and Time UI

---
+++ Assets/Scripts/Game Structures/Game Managers/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    public static int Days = 0;
+    public static float secondsPerDay = 30;
+    private static float currentCount = 0;
+
+    private static Text daysText;
+    private static Image timePanel;
+	// Use this for initialization
+	void Start () {
+        daysText = GameObject.Find("Days Text").GetComponent<Text>();
+        timePanel = GameObject.Find("Time Panel").GetComponent<Image>();
+        daysText.text = Days.ToString();
+	}
+
+    void OnGUI()
+    {
+        Debug.Log((secondsPerDay - currentCount) / secondsPerDay);
+        timePanel.fillAmount = (secondsPerDay - currentCount) / secondsPerDay;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentCount += Time.deltaTime;
+        if (currentCount > secondsPerDay)
+        {
+            Days += 1;
+            daysText.text = Days.ToString();
+            currentCount -= secondsPerDay;
+        }
+	}
+}
