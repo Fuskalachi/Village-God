e5205b3468e53bf198350cc11464eab6199b213d
Fuskalachi <miccelo@gmail.com>
2016-01-31 14:55:06 -0500

    Probably the last commit

--- Assets/Scripts/Game Structures/Workers/Worker.cs
+++ Assets/Scripts/Game Structures/Workers/Worker.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class Worker : MonoBehaviour {
     public bool inMotion;
     public bool startDancing;
+    private new Rigidbody rigidbody;
+    private Animator animator;
 
     private NavMeshAgent agent;
 	// Use this for initialization
 	void Awake () {
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         agent = gameObject.GetComponent<NavMeshAgent>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        inMotion = rigidbody.velocity.x > 0 || rigidbody.velocity.y > 0;
+        animator.SetBool("inMotion", inMotion);
+        if (!inMotion)
+            animator.SetTrigger("startDancing");
 	}
 }
